06ca6860f39880231e7e97b9332f873cb248cfe9
dicorndl <dicorndl@gmail.com>
2021-03-02 12:50:23 +0900

    14.2.1 누가 입찰자에 대해 알고 있는가
    - 최종 가격 제시한 입찰자가 스나이퍼라면 스나이퍼가 낙찰 중임을 알 수 있음.
    - 위 로직을 어디에 넣을지 결정해야함.
    - 스나이퍼가 낙찰 중인지 파악하려면 가격이 도착할 때 이게 내가 제시한 가격인지 알 수 있으면 됨.
    - 이 로직을 판단하는 것은 Translator의 역할에 해당.
    - 실패하는 테스트 작성.

--- src/main/java/auctionsniper/AuctionEventListener.java
+++ src/main/java/auctionsniper/AuctionEventListener.java
@@ -2,6 +2,10 @@ package auctionsniper;
 
 public interface AuctionEventListener {
 
+  enum PriceSource {
+    FromSniper, FromOtherBidder;
+  }
+
   void auctionClosed();
 
   void currentPrice(int price, int increment);
--- src/test/java/auctionsniper/AuctionMessageTranslatorTest.java
+++ src/test/java/auctionsniper/AuctionMessageTranslatorTest.java
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import auctionsniper.AuctionEventListener.PriceSource;
+
 @ExtendWith(MockitoExtension.class)
 public class AuctionMessageTranslatorTest {
 
@@ -32,12 +34,23 @@ public class AuctionMessageTranslatorTest {
   }
 
   @Test
-  void notifiesBidDetailsWhenCurrentPriceMessageReceived() {
+  void notifiesBidDetailsWhenCurrentPriceMessageReceivedFromOtherBidder() {
     Message message = new Message();
-    message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else");
+    message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;");
 
     translator.processMessage(UNUSED_CHAT, message);
 
-    verify(lister, times(1)).currentPrice(192, 7);
+    verify(lister, times(1)).currentPrice(192, 7, PriceSource.FromOtherBidder);
+  }
+
+  @Test
+  void notifiesBidDetailsWhenCurrentPriceMessageReceivedFromSniper() {
+    Message message = new Message();
+    message.setBody(
+        "SOLVersion: 1.1; Event: PRICE; CurrentPrice: 234; Increment: 5; Bidder: " + ApplicationRunner.SNIPER_ID + ";");
+
+    translator.processMessage(UNUSED_CHAT, message);
+
+    verify(lister, times(1)).currentPrice(234, 5, PriceSource.FromSniper);
   }
 }
