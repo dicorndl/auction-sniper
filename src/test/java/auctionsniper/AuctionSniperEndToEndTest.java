0be35cdce8c22008c20cfc6e1665a9fb3dbcd88c
dicorndl <dicorndl@gmail.com>
2021-03-02 12:46:47 +0900

    14.1 우선 실패하는 테스트 작성
    - 새로운 End to End 테스트 작성.

--- src/test/java/auctionsniper/AuctionSniperEndToEndTest.java
+++ src/test/java/auctionsniper/AuctionSniperEndToEndTest.java
@@ -47,6 +47,26 @@ public class AuctionSniperEndToEndTest {
     application.showsSniperHasLostAuction();
   }
 
+  @Test
+  @Order(3)
+  void sniperWinsAnAuctionByBiddingHigher() throws XMPPException, InterruptedException {
+    auction.startSellingItem();
+
+    application.startBiddingIn(auction);
+    auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+
+    auction.reportPrice(1000, 98, "other bidder");
+    application.hasShownSniperIsBidding();
+
+    auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+
+    auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+    application.hasShownSniperIsWinning();
+
+    auction.announceClosed();
+    application.showsSniperHasWonAuction();
+  }
+
   @AfterEach
   void stopAuctionAndApplication() {
     auction.stop();
